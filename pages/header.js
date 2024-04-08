dfd61e35be39e0d1a2b8254856b8ba998ab7a640
I Kha <122979963+jarvismsk@users.noreply.github.com>
2024-04-08 08:33:25 +0000

    modified header

--- pages/header.js
+++ pages/header.js
@@ -23,7 +23,7 @@ const Header = () => {
     <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col sm:flex-row items-center justify-center">
         <a href="tel:7022935544" class="text-white font-semibold  duration-150 hover:text-indigo-100 flex items-center gap-x-1">
             <span class="sm:hidden"> Call us: +91 7022935544</span>
-            <span class="hidden sm:inline"> Enquiry? Call us: </span> <span class="hidden sm:inline">+91 7022935544</span>
+            <span class="hidden sm:inline"> Questions?  -   Call us: </span> <span class="hidden sm:inline">+91 7022935544</span>
         
         </a>
     </div>
